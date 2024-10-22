0b0b781a61001d0817f1a6db8396ddbfd7bb59f3
Matthew Lancellotti <mvlancellotti@gmail.com>
2024-10-21 20:49:15 -0400

    remove commented-out code

--- client-side/main.js
+++ client-side/main.js
@@ -163,7 +163,6 @@ function windowManage(cmds) {
 	// 		account    : (T/F) whether the account window will be shown
 	// 		recipeDesc : (T/F) whether the recipe description window will be shown
 
-
 	if ("account" in cmds) {
 		if (cmds["account"]) {
 			$("#AccountWindow").show()
@@ -197,144 +196,11 @@ function windowManage(cmds) {
 			$("#AccountPanel").hide()
 		}
 	}
-
-
-}
-
-
-/*
-
-///////////////////////// Function to search and display items based on searched items /////////////////////////
-
-
-
-//random menu generator
-function generate() {
-	genNumber=document.getElementById("mealNumValue").value;
-	if (genNumber=='') { //if empty, make the value 7
-		genNumber=7;
-		document.getElementById("mealNumValue").value=genNumber;
-	}
-	// parse specifications:
-	var specText=document.getElementById("mealConditions").value;
-	specText+=',0 side';
-	specText=specText.replace(/\n/g,","); //change "new line" to ","
-	specText=specText.replace(/,\s+|\s+,/g,","); //remove space after and before commas
-	specText=specText.replace(/^\s+|\s+$/g,"");//remove space at the beginning and end
-	specText=specText.replace(/^\s*,|,\s*$/g,"");//remove empty spots at beginning or end
-	specText=specText.replace(/,\s*,/g,",");//remove empty spots in the middle
-
-	var rawSpecList=specText.split(",");//parse into individual specs
-	var specList=new Array;//specList[i]=[min,max,ingr,Number added to final menu]
-	var form1=/(\d+)\s*\/\s*(\d+)\s+(.+)/;
-	var form2=/(\d+)\s+(.+)/;
-	for (var i in rawSpecList) { //parse individual specs into min/max/ingr
-		//if "#1/#2 ingr" is given, assume min#1, max=#2
-		if (form1.test(rawSpecList[i])){
-			specList.push([eval(RegExp.$1),eval(RegExp.$2),RegExp.$3,0])
-		}
-		//if "# ingr" is given, assume min=max=#
-		else if (form2.test(rawSpecList[i])){
-			specList.push([eval(RegExp.$1),eval(RegExp.$1),RegExp.$2,0])
-		}
-		//if "ingr" is given, assume min 1, max equal to genNumber
-		else {
-			specList.push([1,genNumber,rawSpecList[i],0])
-		}
-	}
-
-	for (var i in specList) { //first go through and replace all ingrs with their pointer
-		//if min>max, switch
-		if (specList[i][0]>specList[i][1]){var temp=specList[i][1];
-			specList[i][1]=specList[i][0];specList[i][0]=temp;}
-
-		if (itemListStrings.indexOf(specList[i][2])!=-1) {
-			var itemKey=itemList[itemListStrings.indexOf(specList[i][2])]; //store pointer
-			if (itemKey.type>=2) {specList[i][2]=itemKey;} //if an ingr/description
-			else {specList.splice(i,1)} //otherwise, remove
-		}
-		else {specList.splice(i,1)} //otherwise, remove
-	}
-
-	var possibleMenuItems=itemSearch(itemList,1,-1,1); //all meal items
-	possibleMenuItems=removeMaxes(possibleMenuItems,specList); //remove any 0 max specs
-	foundItemList=new Array();
-	var finalMenuSize=0;
-
-	//first round through to meet all minima
-	for (var i in specList) {
-
-		//find all menu options that contain this ingr
-		var mealOptions=itemSearch(possibleMenuItems,1,specList[i][2],1);
-		//if we need more menu items, there are still possibilities, the min>0, and this is an actual item
-		while (mealOptions.length>0 && finalMenuSize<genNumber && specList[i][0]>specList[i][3]) {
-
-			//add a random option
-			var addedMeal=mealOptions[Math.floor(Math.random()*mealOptions.length)];
-			finalMenuSize=foundItemList.push(addedMeal);
-			addedMeal.found=1;
-
-			//update progress on each spec and remove added meal and any menu items that would exceed any spec maxes
-			specList=tallyIngredients(addedMeal,specList);
-			possibleMenuItems=removeMeal(possibleMenuItems,addedMeal);
-			possibleMenuItems=removeMaxes(possibleMenuItems,specList);
-
-			//find all menu options that contain this ingr
-			mealOptions=itemSearch(possibleMenuItems,1,specList[i][2],1);
-		}
-	}
-
-	//second round, pick randomly from all options while options remain and more menu items are required
-	while (possibleMenuItems.length>0 && finalMenuSize<genNumber) {
-		//add a random option
-		var addedMeal=possibleMenuItems[Math.floor(Math.random()*possibleMenuItems.length)];
-		finalMenuSize=foundItemList.push(addedMeal);
-		addedMeal.found=1;
-
-		//update progress on each spec and remove added meal and any menu items that would exceed any spec maxes
-		specList=tallyIngredients(addedMeal,specList);
-		possibleMenuItems=removeMeal(possibleMenuItems,addedMeal);
-		possibleMenuItems=removeMaxes(possibleMenuItems,specList);
-
-	}
-
-	positionResults(foundItemList,1);
 }
 
 
 
-////////////////////////////////////////////////Save all Data////////////////////////////////////////////////
-//store data in cookie by combining into string
-var savedHistory=[]; //hold history of changes to allow "Undo"
-function saveData() {
-	var itemStore='';
-	for (var i in itemList) {
-		if (itemList[i].type==1) {//only store if this is a menu item (assume no lone ingrs)
-			if(itemStore!='') {
-				itemStore+="|||";
-			}
-			itemStore+=itemList[i].name;
-			itemStore+="||";
-			for (var j in itemList[i].connections) {
-				if (j>0) {itemStore+="|";}
-				itemStore+=itemList[i].connections[j].type+''+itemList[i].connections[j].name; //add type immediately followed by the name
-			}
-		}
-	}
 
-	if (savedHistory[placeInHistory]!=itemStore) {
-		if (placeInHistory>0) {savedHistory.splice(0,placeInHistory);
-			placeInHistory=0;
-			document.getElementById("redoButton").disabled=true;}
-		savedHistory.splice(0,0,itemStore);
-		if (savedHistory.length>20) {savedHistory.splice(20,1);}
-		else if (savedHistory.length==2) {document.getElementById("undoButton").disabled=false;}
-	}
-
-	setCookie("itemStore",itemStore)
-}
-
-*/
 
 
 ////////////////////// MAIN //////////////////////
